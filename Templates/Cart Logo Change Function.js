bf1f0a62ff89f93a5a4074adecfba83b9562da91
139or148 <63042065+139or148@users.noreply.github.com>
2021-04-01 19:03:21 +0800

    Delete Cart Logo Change Function.js

--- Templates/Cart Logo Change Function.js
+++
@@ -1,6 +0,0 @@
-//If any item added in the shopping cart, the shopping cart logo(button) style will change to notification button's  
-
-if (localStorage.getItem("item_list") != null & localStorage.getItem("item_list").split(",").length>0){
-  document.getElementById("shopping_cart_logo_notification").innerHTML = 
-  `<div class="shopping-cart-logo-notification">`+localStorage.getItem("item_list").split(",").length+`</div>`
-}
