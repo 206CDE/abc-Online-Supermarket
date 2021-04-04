9ef3642deba65c99a8d1450b81f9073b6be0c119
139or148 <63042065+139or148@users.noreply.github.com>
2021-04-04 21:51:29 +0800

    Add files via upload

---
+++ Templates/Zoom Resize Function.js
@@ -0,0 +1,64 @@
+//Resize the screen based on any 16:9 resolutions
+
+if (window.screen.width <= 256)
+  {document.body.style.zoom="17%"}
+
+else if (window.screen.width <= 426)
+  {document.body.style.zoom="28%"}	
+
+else if (window.screen.width <= 640)
+  {document.body.style.zoom="42%"}	
+
+else if (window.screen.width <= 768)
+  {document.body.style.zoom="50%"}
+
+else if (window.screen.width <= 800)
+  {document.body.style.zoom="13%"}
+
+else if (window.screen.width <= 854)
+  {document.body.style.zoom="56%"}
+
+else if (window.screen.width <= 960)
+  {document.body.style.zoom="63%"}
+
+else if (window.screen.width <= 1024)
+  {document.body.style.zoom="67%"}
+
+else if (window.screen.width <= 1280)
+  {document.body.style.zoom="84%"}
+
+else if (window.screen.width <= 1366)
+  {document.body.style.zoom="89%"}
+
+else if (window.screen.width <= 1536) //Default of zoom size of testing
+  {document.body.style.zoom="100%"}
+
+else if (window.screen.width <= 1600)
+  {document.body.style.zoom="105%"}
+
+else if (window.screen.width <= 1920)
+  {document.body.style.zoom="125%"}
+
+else if (window.screen.width <= 2048)
+  {document.body.style.zoom="134%"}
+
+else if (window.screen.width <= 2560)
+  {document.body.style.zoom="167%"}
+
+else if (window.screen.width <= 2880)
+  {document.body.style.zoom="188%"}
+
+else if (window.screen.width <= 3200)
+  {document.body.style.zoom="209%"}
+
+else if (window.screen.width <= 3840)
+  {document.body.style.zoom="250%"}
+
+else if (window.screen.width <= 4096)
+  {document.body.style.zoom="267%"}
+
+else if (window.screen.width <= 5120)
+  {document.body.style.zoom="334%"}
+
+else if (window.screen.width <= 7680)
+  {document.body.style.zoom="500%"}
