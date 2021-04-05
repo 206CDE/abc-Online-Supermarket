6ae38a3002f425e9d390921a722e947b86b755b8
139or148 <63042065+139or148@users.noreply.github.com>
2021-04-06 03:48:05 +0800

    Update Database_Function.py

--- Database_Function.py
+++ Database_Function.py
@@ -9,6 +9,7 @@ from flask import Flask, render_template, redirect, url_for, request
 DB = pymysql.connect(
   host = "139.0.0.148",
   user = "abc_Supermarket",
+  password = "E79G3T07J2",
   database = "abc supermarket database"
 )
 CUR = DB.cursor() 
