0c66a5ec7976feb5ed1fb031610203c638efd5e7
Samson-Ng-test <81677327+Samson-Ng-test@users.noreply.github.com>
2021-04-07 19:28:15 +0800

    Update Database_Function.py

--- Database_Function.py
+++ Database_Function.py
@@ -7,12 +7,11 @@ from flask import Flask, render_template, redirect, url_for, request
 
 #Connect to the database
 DB = pymysql.connect(
-  host = "0.0.0.0",  
+  host = "127.0.0.1",  
   port = 3306,
-  user = "abc_Supermarket",
-  password = "E79G3T07J2",  
-
-  #database = "abc supermarket database"
+  user = "root",
+  password = "",  
+  database = "abc supermarket database"
 )
 CUR = DB.cursor() 
 
